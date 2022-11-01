c4f2d59561f3e751dde0e325efe6ffc946c6239e
MatthewKlayme <54646864+MatthewKlayme@users.noreply.github.com>
2022-11-01 15:32:00 -0500

    Books create/index/new

--- server.js
+++ server.js
@@ -4,24 +4,30 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL,{
+mongoose.connect(process.env.DATABASE_URL || "https://localhost:3000",{
     useNewUrlParser: true
 })
-const db = mongoose.connection;
-db.on('error', error => console.error(error))
+const db = mongoose.connection
+db.on('error', error => console.log("This is THE FUCKING ERROR"+error))
 db.once('open', () => console.log("Connected to Mongoose"))
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 const port = process.env.PORT;
 app.listen(port || 3000);
